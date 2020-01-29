3cc5ed0af4eaf33c6f1776bf188c0480ca8dca25
André Muller Cascadan <cascadan@ncc.unesp.br>
2020-01-29 17:16:40 -0300

    Fix Year and Full Name in the license text.

--- bb_i2c_master.c
+++ bb_i2c_master.c
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) [2020] [André Muller Cascadan]
+Copyright (c) 2020 André Muller Cascadan
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
